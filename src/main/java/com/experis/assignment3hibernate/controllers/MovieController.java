d695373ccc85a88d80083489ef5edc1ff40c19fb
Hamza Hayd <43278171+Hamza1001101@users.noreply.github.com>
2021-07-29 09:36:10 +0200

    Update MovieController.java

--- src/main/java/com/experis/assignment3hibernate/controllers/MovieController.java
+++ src/main/java/com/experis/assignment3hibernate/controllers/MovieController.java
@@ -9,11 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/movies")
-
 public class MovieController {
 
     @Autowired
@@ -26,8 +27,55 @@ public class MovieController {
         return new ResponseEntity<>(data, status);
     }
 
+    @PostMapping
+    public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
+        Movie add = movieRepository.save(movie);
+        HttpStatus status;
+        status = HttpStatus.CREATED;
+        // Return a location -> url to get the new resource
+        return new ResponseEntity<>(add, status);
+    }
 
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getSpecificMovie(@PathVariable Long id) {
+        HttpStatus status;
+        Movie add = new Movie();
+        if (!movieRepository.existsById(id)) {
+            status = HttpStatus.NOT_FOUND;
+            return new ResponseEntity<>(add, status);
+        }
+        add = movieRepository.findById(id).get();
+        status = HttpStatus.OK;
+        return new ResponseEntity<>(add, status);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable("id") Long id, @RequestBody Movie movie) {
+        Optional<Movie> movieData = movieRepository.findById(id);
+
+        if (movieData.isPresent()) {
+            Movie _movie = movieData.get();
+            _movie.setTitle(movie.getTitle());
+            _movie.setDirector(movie.getDirector());
+            _movie.setGenre(movie.getGenre());
+            _movie.setReleaseYear(movie.getReleaseYear());
+            _movie.setTrailer(movie.getTrailer());
+            _movie.setPhoto(movie.getPhoto());
+
+            return new ResponseEntity<>(movieRepository.save(_movie), HttpStatus.OK);
+        } else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteMovie(@PathVariable("id") long id) {
+        try {
+            movieRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
